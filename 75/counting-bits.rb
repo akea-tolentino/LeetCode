6d543e9978b27a9eecf0a6bd64869819f498d73b
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-09-01 15:21:35 -0700

    optimize counting bits ruby

--- 75/counting-bits.rb
+++ 75/counting-bits.rb
@@ -22,34 +22,60 @@
 # Constraints:
 # 0 <= n <= 105
 
-def count_bits(n)
+def count_bits(n) #optimized
     #create an array to push values into
-    ans = [0]
+    ans = [0, 1]
 
-    #set a base case
-    return ans if n == 0
+    #set base cases
+    return [0] if n == 0
+    return ans if n == 1
 
     #set a pointer
-    i = 1
+    i = 2
+
+    temp = [1]
 
     #while loop for while pointer is not greater than n
     while i <= n
         # calculate number of 1's in binary representation
-        bstring = i.to_s(2)
-        
-        # counter for 1's
-        i_in_string = 0
-
-        # iterate through binary string and increment counter by one for every '1' read
-        bstring.each_char do |bin|
-            i_in_string += 1 if bin == '1'
-        end
-
-        # push value into ans array, increment pointer
-        ans.push(i_in_string)
-        i += 1
+        ans.concat(temp.concat(temp.map { |num| num + 1 }))
+        i *= 2
     end
+
+    ans.pop(i - n - 1)
     
     # return ans array
     return ans
 end
+
+# def count_bits(n)
+#     #create an array to push values into
+#     ans = [0]
+
+#     #set a base case
+#     return ans if n == 0
+
+#     #set a pointer
+#     i = 1
+
+#     #while loop for while pointer is not greater than n
+#     while i <= n
+#         # calculate number of 1's in binary representation
+#         bstring = i.to_s(2)
+        
+#         # counter for 1's
+#         i_in_string = 0
+
+#         # iterate through binary string and increment counter by one for every '1' read
+#         bstring.each_char do |bin|
+#             i_in_string += 1 if bin == '1'
+#         end
+
+#         # push value into ans array, increment pointer
+#         ans.push(i_in_string)
+#         i += 1
+#     end
+    
+#     # return ans array
+#     return ans
+# end
