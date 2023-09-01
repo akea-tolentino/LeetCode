2ce1733b9fb6451e0f37a44afaa5fc5df7c0155d
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-09-01 12:51:39 -0700

    solve counting bits ruby

--- 75/counting-bits.rb
+++ 75/counting-bits.rb
@@ -21,3 +21,35 @@
 
 # Constraints:
 # 0 <= n <= 105
+
+def count_bits(n)
+    #create an array to push values into
+    ans = [0]
+
+    #set a base case
+    return ans if n == 0
+
+    #set a pointer
+    i = 1
+
+    #while loop for while pointer is not greater than n
+    while i <= n
+        # calculate number of 1's in binary representation
+        bstring = i.to_s(2)
+        
+        # counter for 1's
+        i_in_string = 0
+
+        # iterate through binary string and increment counter by one for every '1' read
+        bstring.each_char do |bin|
+            i_in_string += 1 if bin == '1'
+        end
+
+        # push value into ans array, increment pointer
+        ans.push(i_in_string)
+        i += 1
+    end
+    
+    # return ans array
+    return ans
+end
