e28300c0d415e21b8bd69f1a5ca090cc158b3ecd
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-09-07 14:48:32 -0700

    solve valid anagram rb

--- 75/valid-anagram.rb
+++ 75/valid-anagram.rb
@@ -13,3 +13,26 @@
 # Constraints:
 # 1 <= s.length, t.length <= 5 * 104
 # s and t consist of lowercase English letters.
+
+def is_anagram(s, t)
+    # check string lengths, if !=, then false
+    return false if s.length != t.length
+
+    # iterate through s, populating a hash counter for characters
+    char_count = Hash.new(0)
+
+    s.each_char do |char|
+        char_count[char] += 1
+    end
+
+    # iterate through t, decrementing hash counter for each value, if value doesnt exist, return false
+    t.each_char do |char|
+        if char_count[char] >= 1
+            char_count[char] -= 1
+        else
+            return false
+        end
+    end
+
+    true
+end
