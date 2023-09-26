7927d4e0a5988002e68823844dcfc4039ef84b1a
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-09-26 13:34:48 -0700

    solve linked list cycle py

--- 75/linked-list-cycle.py
+++ 75/linked-list-cycle.py
@@ -25,3 +25,18 @@
 # The number of the nodes in the list is in the range [0, 104].
 # -105 <= Node.val <= 105
 # pos is -1 or a valid index in the linked-list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
